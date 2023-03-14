8291dbd2ad8ab0cbb733f1dee27f1811f2894599
krushna Gavali <krushnagawali744@gmail.com>
2023-03-15 00:21:05 +0530

    Create 002_Printing_Statemanets.java

---
+++ 002_Printing_Statemanets.java
@@ -0,0 +1,8 @@
+class PrintingStatements
+{
+  public static void main(string[] args)
+  {
+    System.out.println("Hello world"); // print statement next line
+    System.out.print("Hello world");   // print statement on same line 
+  }
+}
