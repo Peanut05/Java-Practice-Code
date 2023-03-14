6dc5797d0d5ff85b434bbd7cb9b2a55f3e35d06f
krushna Gavali <krushnagawali744@gmail.com>
2023-03-14 15:22:38 +0530

    Create 001_First.java

---
+++ 001_First.java
@@ -0,0 +1,7 @@
+Class First // class 
+{
+  Public static void main(string[] args) // main method where starts the execution of java program
+  {
+    System.out.println("Hello World");  // Printing Statement
+  }
+}
