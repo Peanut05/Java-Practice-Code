b887c2c1f186ecccb58f4adfdc2695b98e3fc45c
krushna Gavali <krushnagawali744@gmail.com>
2023-03-22 00:20:45 +0530

    Update 004_Print_Stored_values.java

--- 004_Print_Stored_values.java
+++ 004_Print_Stored_values.java
@@ -4,6 +4,8 @@ class PrintingValues
   string email = "peanut005@gmail.com" ;
   float persantages = 89.60 ;
   int age = 23 ;
+  long mobileNumber = 3948292032 ;
   
+  System.out.println("Name Is :" +name);
   
 }
