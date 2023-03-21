6ff8b5ade97fec43ff10cf2916ef25df6f0c4c3d
krushna Gavali <krushnagawali744@gmail.com>
2023-03-22 00:32:37 +0530

    Create 005_PlusOprator.java

---
+++ 005_PlusOprator.java
@@ -0,0 +1,12 @@
+Class PlusOprator
+{
+  Public static void main(String args[])
+  {
+    System.out.println(10+12);                 // it will print addition of 10+12 that is 22
+    System.out.println("Hello "+"World");      // it will print the concatnation of two strings 
+    int a = 10;
+    int b = 10;
+    int add = a+b;
+    System.out.println("Addition is :"+add);  // it will concat int value into string and print whole string 
+  }
+}
