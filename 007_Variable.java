feff1b1fabd4f349990bddacb684a6ffde5bb8cb
krushna Gavali <krushnagawali744@gmail.com>
2023-03-24 16:04:43 +0530

    Create 007_Variable.java

---
+++ 007_Variable.java
@@ -0,0 +1,20 @@
+/* Variables :==>> (*) Variables are container to store program data.
+                   (*) Program data is nouthing but the data (values) used inside program.
+                   (*) Variable is not a perment storage (Variable is temprory storage).
+                   (*) The memory woll be allowcated to the variable at time of execution of a program && the memory will be allowcated at the time of termination of a program.
+                   
+*/
+
+class program1
+{
+  Public static void main(Strings[] args)
+  {
+    int a1  ; // Declaration of variable ....
+    a1 = 10 ; // Initialization of variable ....
+    
+    int a2 = 20 ; // Declaration and Initialization at a time ....
+    
+    int a1 = 101; // reinitialization .... initialize value/litarals again to variable ....
+  }
+  
+}
