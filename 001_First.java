54f1e8fb2b2855c2733b87b5fdf083c425760ce5
krushna Gavali <krushnagawali744@gmail.com>
2023-03-24 15:20:06 +0530

    Update 001_First.java

--- 001_First.java
+++ 001_First.java
@@ -1,6 +1,6 @@
 Class First // class 
 {
-  Public static void main(string args[]) // main method where starts the execution of java program
+  Public static void main(string[] args) // main method where starts the execution of java program
   {
     System.out.println("Hello World");  // Printing Statement
   }
