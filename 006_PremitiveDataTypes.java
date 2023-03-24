0f9e4d9047004c8c665abf2c9186f1de57595c62
krushna Gavali <krushnagawali744@gmail.com>
2023-03-24 15:52:24 +0530

    Create 006_PremitiveDataTypes.java

---
+++ 006_PremitiveDataTypes.java
@@ -0,0 +1,25 @@
+class PremitiveDataTypes
+{
+  // Numbers == 1] Byte  (8 bit)
+  //            2] short (2 bytes)
+  //            3] int   (4 bytes)
+  //            4] long  (8 bytes)
+  
+  // Decimal == 1] float (4 bytes)
+  //            2] Double(8 bytes)
+  
+  // Charecter == 1] char (1 byte)
+  
+  // Boolean == 1] true / false (1 bit)
+  
+  
+  int num1 = 10;
+  float num2 = 10.0;
+  char ch = 'A';
+  boolean b = true ; 
+  
+  System.out.println(num1);
+  System.out.println(num2);
+  System.out.println(ch);
+  System.out.println(b);
+}
