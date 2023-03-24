21a98d39c6957e3ba8fa4f80be6dac3d101bda83
krushna Gavali <krushnagawali744@gmail.com>
2023-03-24 16:56:58 +0530

    Create 008_DataTypeCasting.java

---
+++ 008_DataTypeCasting.java
@@ -0,0 +1,19 @@
+/* Data Typecasting :==>> (*) Data typecasting is also known as premitive casting.
+                          (*) Premitive casting is process to converting one primitive to another premitive is known as premitivecasting 
+                          (*) Its devied into two types : [1] Winding :==   (*) winding is process to converting lower type to higher type ...
+                                                                            (*) Winding is also called automatic cating ...
+                                                          [2] Narrowing :== (*) Narrowing is the procerss of converting a higher type of data into lower type ...
+                                                                            (*) Narrowing is also called forceful casting ...
+                                                                            (*) In narrowing is a chance of data loss ...
+*/
+class DataTypeCasting
+  {
+  Public static void main(String[] args)
+  {
+    int a = 10;
+    double d = a;  // winding that is automatic casting ....
+    
+    
+    
+  }
+  }
