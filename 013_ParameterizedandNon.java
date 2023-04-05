349a4ca05bb5cea4be7210246586288ef77c931e
krushna Gavali <krushnagawali744@gmail.com>
2023-04-05 23:51:48 +0530

    Update 013_ParameterizedandNon.java

--- 013_ParameterizedandNon.java
+++ 013_ParameterizedandNon.java
@@ -2,6 +2,9 @@
 
 // Input parameter :==>>
 /*
-  1) Input parameter are nothing but variable declared inside a method signature .
-  2) We can use input parameters to pass the runtime data for specific method .
+   1) Input parameter are nothing but variable declared inside a method signature .
+   2) We can use input parameters to pass the runtime data for specific method .
 */
+
+// Zero Parameterized Method :=>>
+
