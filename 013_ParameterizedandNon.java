824878cab4043786b495d113bb593f4873013982
krushna Gavali <krushnagawali744@gmail.com>
2023-04-05 23:58:23 +0530

    Update 013_ParameterizedandNon.java

--- 013_ParameterizedandNon.java
+++ 013_ParameterizedandNon.java
@@ -1,16 +1,22 @@
 // Zero Parameterised and Parameterised Method in java : ==>>>
 
 // Input parameter :==>>
+
 /*
    1) Input parameter are nothing but variable declared inside a method signature .
    2) We can use input parameters to pass the runtime data for specific method .
 */
 
 // Zero Parameterized Method :=>>
+
 /*
    1) The method which is declared without input parameters are known as zero parameterized method .
    2) If method are zero parameterized we cannot pass any runtime data for specific method .
    3) Zero parameterized method is also known as non-parameterized method .
-   
+*/
+
+// Parameterized Method :==>>
+/*
+   1) If the method accept input parameter the method will be known as parameterized method.
 */
 
