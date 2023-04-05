e7310edbcceccee35ad6d6759a7683dda4cabb39
krushna Gavali <krushnagawali744@gmail.com>
2023-04-05 23:16:08 +0530

    Update 011_MethodChanging.java

--- 011_MethodChanging.java
+++ 011_MethodChanging.java
@@ -1,1 +1,28 @@
-.
+/*
+Method Chaining :==>>>
+  1] The process of calling one externel method inside another externel method and that externel method get called from main method
+     is called method chaining .
+  2] In method chaining we can call multipal external methods inside one external method and that externel method called inside main
+     main method .
+  */
+
+class MethodChaining
+{
+  public static void text()
+  {
+    System.out.println("Text Start ...");
+    System.out.println("Text End ...");
+  }
+  public static void show()
+  {
+    System.out.println("Show Start ...");
+    text();
+    System.out.println("Show End ...");
+  }
+  public static void main(String[] args)
+  {
+    System.out.println("Main Start ...");
+    show();
+    System.out.println("Main End ...");
+  }
+}
